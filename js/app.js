1b48fbd022622f7cb41bc77380272e39d3740864
Amalia <amaliapdvh@gmail.com>
2021-02-15 23:35:56 +0100

    js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,29 @@
 
 const valoracionIn  = document.querySelector('#valoracionIn')
 const valoracionBtn = document.querySelector('#valoracionBtn')
+const paisIn        = document.querySelector('#paisIn')
+const controlIn     = document.querySelector('#controlIn')
+const entidadIn     = document.querySelector('#entidadIn')
+const sucursalIn    = document.querySelector('#sucursalIn')
+const dcIn          = document.querySelector('#dcIn')
+const cuentaIn      = document.querySelector('#cuentaIn')
+const cuentaBtn     = document.querySelector('#cuentaBtn')
+const dateIn        = document.querySelector('#dateIn')
+const dateBtn       = document.querySelector('#dateBtn')
 
-valoracionBtn.addEventListener('click',()=>{
-	alert(`has valorado con ${valoracionIn.value} puntos`)
+valoracionBtn.addEventListener('click',function(){
+	alert(`has valorado con ${valoracionIn.value} puntos.`)
+})
+
+cuentaBtn.addEventListener('click', function(){
+	if(cuentaIn.value === ''){
+		alert(`Rellene los campos indicados`)
+	}else{
+		alert(`le informamos de que el numero de cuenta es: ${cuentaIn.value}`)
+	}
 })
 
 
+dateBtn.addEventListener('click', function(){
+	alert(`El dia de la realizaxción de la encuesta es: ${dateIn.value}`)
+})
